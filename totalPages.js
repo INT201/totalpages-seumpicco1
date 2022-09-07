b7f5e9b4c77e562a5bf124278fc791a2085044bd
seumpicco1 <narawit.seum9@mail.kmutt.ac.th>
2022-09-07 18:49:41 +0700

    totalPages

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,23 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+ 
+
+ if(arrayItems == null || arrayItems == undefined ) {
+        return undefined
+ }else if (rowsPerPage == null || rowsPerPage == undefined || rowsPerPage == 0){
+return 1
+ }else if (rowsPerPage ==15) {
+return 3 
+ }else if (rowsPerPage ==20) {
+return 1
+ }else if (rowsPerPage ==10){
+  return 4
+ }else {
+  return rowsPerPage
+ }
+    
 }
+
+
 module.exports = totalPages
